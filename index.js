b5c37f573392ade1e2bf6d749d6ca132c2727d3d
basbilijam <basbilijam@gmail.com>
2017-02-23 12:12:27 +0100

    Add and delete function

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// declaring and requiring npm packages for this app
+
 const Sequelize = require('sequelize'),
 			express = require('express'),
 			pug = require('pug'),
@@ -5,49 +7,78 @@ const Sequelize = require('sequelize'),
 			bodyParser = require('body-parser'),
 			methodOverride = require('method-override');
 
+// declaring that we are using express in this app
+// connecting to database bulletinboard, using postgres, accessing user and password from bash profile
 var app = express(),
 		sequelize = new Sequelize('bulletinboard', process.env.POSTGRES_USER, process.env.POSTGRES_PASSWORD, {
 		dialect: 'postgres'
 		});
 
-// var noticesRouter = require('routes/notices');
+// requiting route notices.js in routes folder
 
+var noticesRouter = require('./routes/notices');
+
+// creating a table 'notice' with two columns 'title' and 'body'
 var notice = sequelize.define('notice', {
 	title: Sequelize.STRING,
 	body: Sequelize.TEXT
 });
 
+// using morgan middleware to print HTTP request to help debugging (no need to write console.logs)
 app.use(morgan('dev'));
 
-// use bodyParser for input in forms
+// use bodyParser to parse text input in forms
 app.use(bodyParser.urlencoded({extended:false}));
 
-//  delete function
-/* app.use(methodOverride)(req, res) => {
-	if (req.body && typeof req.body === 'object' && '_method' in req.body) {
-		// all three must be true
-		var method = req.body._method;
-		delete req.body._method;
-		return method;
-	}})
-);
-*/
+// use methodOverride to add delete functionality to application
+app.use(methodOverride(function (req, res) {
+  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+    // look in urlencoded POST bodies and delete it
+    var method = req.body._method
+    delete req.body._method
+    return method
+		}})
+	);
 
+// setting 'pug' as the view engine, pug files render to html
 app.set('view engine', 'pug');
 
-// app.use('notices', noticesRouter);
-
+// get request to redirect the homepage to the notices route (in notices.js)
+//1. go to route notices.js, go to the app.get('/') in the notices.js file
 app.get('/', (request, response) => {
-	response.render('notices/add-message.pug');
+	response.redirect('/notices');
 });
 
-app.post('/notice', (request, response) => {
+// get request to path '/board' route
+app.get('/board', (request, response) => {
+	//findAll - native method in sequelize returns everything in notice table
+	// 'notices' object is passed as a parameter, to be rendered in the pug file
+  notice.findAll().then((notices) => {
+		// rendering the board pug file, in the notices folder
+    response.render('notices/board', { notices: notices });
+  });
+});
+
+// post request to post a new entry to noticeboard - a new row in the notices table
+app.post('/notices', (request, response) => {
+	// request.body is the input in the forms (title + body), takes everything in request.body from name attribute
+	// in pug file
+	//create is native to sequalize, creates a new entry into notices table with a promise
 	notice.create(request.body).then(() => {
-		response.redirect('/notices');
+		//redirects to url [home]/board
+		response.redirect('/board');
 	});
 });
 
-// run server
-app.listen(3000, function() {
- console.log('Web server started on port 3000');
+// '/notices' is the url to set app to use noticesRouter variable
+// not clear what this does yet!!
+app.use('/notices', noticesRouter);
+
+// promise - connects to data with sequelize
+sequelize.sync().then(() => {
+  console.log('Connected to database');
+	// creates a server
+  app.listen(3000, () => {
+    console.log('Web Server is running on port 3000');
+  });
 });
